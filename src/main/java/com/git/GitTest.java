662d4847b44d7b7788e55efda5d834031102886a
Xia <Xia@163.com>
2021-10-10 23:45:45 +0800

    hot-fix 3 commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,8 +6,10 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world   1 master");
         System.out.println("hello,world   2");
         System.out.println("hello,world   3   hot fix");
         System.out.println("hello,world   4   hot fix");
+        System.out.println("hello,world   5   hot fix");
     }
 }
