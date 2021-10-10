5f935d52ca270d138a2f0da79662dc2058b57789
Xia <Xia@163.com>
2021-10-10 23:30:21 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,5 +6,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world   2");
     }
 }
