5f598b3f117f5310c90e94727ba0940b675db21a
xia <xia@163.com>
2022-11-21 17:56:29 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,14 +6,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,world");
-        System.out.println("hello,world   1 master");
-        System.out.println("hello,world   2");
-        System.out.println("hello,world   3   hot fix");
-        System.out.println("hello,world   4   hot fix");
-        System.out.println("hello,world   5   hot fix");
-        System.out.println("hello,world   push");
-        System.out.println("hello,world   gitEE  push");
-        System.out.println("hello,world   gitEE  push");
         System.out.println("修改第一次");
     }
 }
